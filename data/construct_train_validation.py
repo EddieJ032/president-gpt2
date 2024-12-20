848d8a641291d319d0083e527d2fd2de8ca47936
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-12-20 07:26:26 -0800

    improve data cleaning and shuffle before splitting

--- data/construct_train_validation.py
+++ data/construct_train_validation.py
@@ -1,12 +1,17 @@
 from collections import defaultdict
+from pathlib import Path
+import random
+import sys
 import pandas as pd
 import pickle as pkl
 
 import torch
 
+sys.path.append(str(Path(__file__).resolve().parent.parent))
+
 from const import presidents, block_size
 
-with open("pres_tokenizer.pkl", "rb") as f:
+with open("../tokenizer/pres_tokenizer.pkl", "rb") as f:
     pres_enc = pkl.load(f)
 
 tokenizer = pres_enc
@@ -20,14 +25,17 @@ pres_df['speech'] = pres_df['speech'].str.replace(r"\[.*?\]", " ", regex=True)
 # ignore everything in between an open and close parentheses
 pres_df['speech'] = pres_df['speech'].str.replace(r"\(.*?\)", " ", regex=True)
 
-# remove duplicate spaces
-pres_df['speech'] = pres_df['speech'].str.replace(r'\s+', ' ', regex=True)
-
 # remove duplicate single quotes
 pres_df['speech'] = pres_df['speech'].str.replace(r"'{2,}", "'", regex=True)
 
 # remove duplicate dash
 pres_df['speech'] = pres_df['speech'].str.replace(r"-{2,}", "-", regex=True)
+
+# add space after every punctuation
+pres_df['speech'] = pres_df['speech'].str.replace(r"([.,;:!?])", r"\1 ", regex=True)
+
+# remove duplicate spaces
+pres_df['speech'] = pres_df['speech'].str.replace(r'\s+', ' ', regex=True)
 
 pres_df['speech_tokenized'] = pres_df['speech'].apply(lambda x: tokenizer.encode(x))
 
@@ -82,8 +90,12 @@ train = []
 validation = []
 
 for president, tensor_list in samples.items():
+    random.shuffle(tensor_list)
     train.extend(tensor_list[:int(0.8*len(tensor_list))])
     validation.extend(tensor_list[int(0.8*len(tensor_list)):])
+    
+print('train len', len(train))
+print('val len', len(validation))
     
 with open('train.pkl', 'wb') as f:
     pkl.dump(train, f)
